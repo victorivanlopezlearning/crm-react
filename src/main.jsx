2953adcb34b62bfaf600ae8f7b42491f9889373f
Víctor Iván López González <victor_ivan2211@hotmail.com>
2023-07-03 12:16:23 -0600

    react router DOM init

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom' // createBrowserRouter define un router por medio de objetos
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <h1 className='text-3xl font-bold'>CRM</h1>,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
