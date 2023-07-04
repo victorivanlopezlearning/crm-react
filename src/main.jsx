6d2b9e9eb8eb268177f8e68cf875a0cab3539806
Víctor Iván López González <victor_ivan2211@hotmail.com>
2023-07-04 15:36:36 -0600

    Add: JSON Server

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Layout from './components/Layout';
-import NewClient, { action, action as actionNewClient } from './pages/new-client';
+import NewClient, { action as actionNewClient } from './pages/new-client';
 import Index, { loader as clientsLoader} from './pages';
 
 
